a77f2cdd5d0c4dfdb88421042cb83868913da2f7
Gutierrez <magalyg@seas.harvard.edu>
2018-05-01 14:40:46 -0400

    Updates on algorithm

--- stub.py
+++ stub.py
@@ -15,24 +15,31 @@ class Learner(object):
         self.last_state  = None
         self.last_action = None
         self.last_reward = None
+        self.theta = np.ones(15)
 
     def reset(self):
         self.last_state  = None
         self.last_action = None
         self.last_reward = None
     #This function is where we update the reward I am just confused as to what to 
-    def _get_reward(s,a):
-       reward =0
-       next_state = s 
-       if(a=0){
-        
-
-        reward = 
-        }else if (a =1){
-        reward = 
-        }
-        return reward 
-  
+    def _get_reward(self,s,a):
+        reward =0 
+        if(a==0):
+            if(s['monkey']['bot']-s['monkey']['vel']<= 0):
+                reward = -10
+            if(s['monkey']['bot']-s['monkey']['vel']<=s['tree']['bot'] or s['monkey']['top']+s['monkey']['vel']>=s['tree']['top']):
+                reward = -5
+            else:
+                reward = 1
+        elif (a ==1):
+            if(s['monkey']['top']+s['monkey']['vel']>=s['tree']['top']):
+                reward = -10
+            if(s['monkey']['bot']+s['monkey']['vel']<=s['tree']['bot'] or s['monkey']['top']-s['monkey']['vel']>=s['tree']['top']):
+                reward = -5
+            else:
+                reward = 1
+        return reward   
+    
     def action_callback(self, state):
         '''
         Implement this function to learn things and take actions.
@@ -43,26 +50,44 @@ class Learner(object):
         #phi_i represetnt the column of a matrix 
         #We need to define the states for a selected number of n not all states because it is continu0s
         #0 MEANS STAY #1 MEANS JUMP 
-        alpha = 
+        alpha = 0.002
+        beta = .05
+        gamma = 0.0001
+
         p_1 = [state.get('score')]
         tree = list(state.get('tree').values())
         monkey = list(state.get('monkey').values())
-        phi_1 = np.hstack((np.zeros(7),p_1,tree,monkey,1))
-        phi_2 = np.hstack((p_1,tree,monkey,np.zeros(7),1))
-        theta =np.ones(15)
+        phi_0 = np.hstack((np.zeros(7),p_1,tree,monkey,1))
+        phi_1 = np.hstack((p_1,tree,monkey,np.zeros(7),1))
+        theta =self.theta
         state_i = np.hstack((p_1,tree,monkey,p_1,tree,monkey,1))
-        beta = .5
-        Q= np.vstack((np.multiply(theta,phi_1), np.multiply(theta,phi_2))).T
-      
-        #It doesnt matter which one is s' or s in any case we could jsut change this to self.reward + gamma*max(self.get_reward(s',a))
-        Q_plus = self.last_reward + gamma*max((self.get_reward(s,0),self.get_reward(s,1)))
-        phi_i =# this one would be defined as the maximum of any of the above 
-        theta_new = theta - alpha *(Q_plus_i - Q)*phi_i
-        Q_new =  np.vstack((np.multiply(theta_new,phi_1), np.multiply(theta_new,phi_2))).T
-        new_action = npr.rand() < 0.1
-        new_state  = state
+        #print(state_i)
+        #print("theta 0", (np.multiply(theta,phi_0)))
+        #print("theta 1", np.multiply(theta,phi_1))
+        Q= np.vstack((np.multiply(theta,phi_0), np.multiply(theta,phi_1))).T
+        #print(Q)
+        rew_0 = self._get_reward(state,0)
+        print(rew_0)
+        rew_1 = self._get_reward(state,1)
+        print(rew_1)
+        if(self.last_reward==None):
+            last_reward =0
+        else:
+            last_reward = self.last_reward
+        Q_plus = last_reward + gamma*max((rew_0,rew_1))
+        if(rew_0>rew_1):
+            phi_i = Q[:, 0]    
+        else:
+            phi_i = Q[:, 1]
 
+        theta_new = theta - alpha *np.dot((Q_plus-phi_i),phi_i)
+        Q_new =  np.vstack((np.multiply(theta_new,phi_0), np.multiply(theta_new,phi_1))).T
+        #print(np.dot(Q_new.T,state_i))
+        new_action = np.argmax(np.dot(Q_new.T,state_i))
+        new_state  = state
+        self.theta = theta_new
         self.last_action = new_action
+        print("new_action", new_action)
         self.last_state  = new_state
 
         return self.last_action
